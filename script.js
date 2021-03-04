65e5f31b3a2d4faf2f47cc31fd12cabddb362433
adz-313 <ads73818@gmail.com>
2021-03-04 07:32:17 +0530

    Fixed submodule issues

--- script.js
+++
@@ -1,8 +0,0 @@
-$('#myTab a').on('click', function (e) {
-    e.preventDefault();
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        e.relatedTarget.classList.remove("active");
-        e.relatedTarget.classList.remove("show");
-    });
-    $(this).tab('show');
-})
